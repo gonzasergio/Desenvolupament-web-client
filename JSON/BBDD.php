032f1423c6bfde3f15fd949984ae3725141ec2b1
gonzasergio <46519009+gonzasergio@users.noreply.github.com>
2019-11-13 12:32:03 +0100

    Delete BBDD.php

--- JSON/BBDD.php
+++
@@ -1,9 +0,0 @@
-<?php
-function select($host, $user, $pass, $bbdd, $tabla){
-    $conn = new mysqli($host, $user, $pass, $bbdd);
-    $stmt = $conn->prepare("SELECT * FROM $tabla");
-    $stmt->execute();
-    $result = $stmt->get_result();
-    return $result;
-}
-?>
